d9198aa2257a111ccd2d6ba4bf0deb0913e1d52d
achugo <achugo2017@gmail>
2019-06-28 18:52:57 +0100

    implement api call to get weather condition

---
+++ js/forecast.js
@@ -0,0 +1,31 @@
+const key = 'UhvqxlJmYxqmBRAAV4UWLcBffarzTZpY';
+
+//http://dataservice.accuweather.com/currentconditions/v1/
+const getWeather = async (id) => {
+
+const baseurl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+const query = `${id}?apikey=${key}`;
+
+const response = await fetch(baseurl + query)
+const data = await response.json();
+return data[0];
+
+}
+const getCity = async (city) => {
+    const baseurl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&&q=${city}`;
+
+    const response = await fetch(baseurl + query);
+    const data = await response.json()
+    return data[0];
+    
+}
+
+
+
+getCity('london')
+    .then(data => {
+        return getWeather(data.Key)
+    }).then((data) => {
+        console.log(data)})
+    .catch((err) => console.log(err))
