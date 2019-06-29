489e0151700835f0e6d47494d7fd90cfbac7c066
achugo <achugo2017@gmail>
2019-06-29 18:15:58 +0100

    add-images to update weather img src and icon

--- js/app.js
+++ js/app.js
@@ -7,11 +7,14 @@ const updateUI = (data) => {
     // const weather = data.weather;
     // console.log(cityDets);
     // console.log(weather)
+    console.log(data)
+    //destructuring the needed properties from data object
+    const {city, weather} = data;
 
-    
+
     //update template
     details.innerHTML = `
-        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <h5 class="my-3">${city.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
         <div class="display-5 my-4">
             <span>${weather.Temperature.Metric.Value}</span>
